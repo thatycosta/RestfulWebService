dab5a9121e886ed8c73d57944920d64fa7088620
Tauane Costa <tauanedasilvacosta@gmail.com>
2020-05-07 19:13:26 -0300

    inserting modifications to token service

--- src/main/java/com/example/myproject/myproject/MyprojectApplication.java
+++ src/main/java/com/example/myproject/myproject/MyprojectApplication.java
@@ -1,5 +1,6 @@
 package com.example.myproject.myproject;
 
+import com.example.myproject.myproject.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +21,10 @@ public class MyprojectApplication {
 	@Bean
 	public SpringApplicationContext springApplicationContext(){
 		return new SpringApplicationContext();
+	}
+
+	@Bean(name = "AppProperties")
+	public AppProperties getAppProperties(){
+		return new AppProperties();
 	}
 }
--- src/main/java/com/example/myproject/myproject/security/SecurityConstants.java
+++ src/main/java/com/example/myproject/myproject/security/SecurityConstants.java
@@ -1,10 +1,19 @@
 package com.example.myproject.myproject.security;
 
+
+import com.example.myproject.myproject.SpringApplicationContext;
+
 public class SecurityConstants {
 
     public static final long EXPIRATION_TIME = 864000000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
+
+
+    public static String getTokenSecret(){
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTokenSecret();
+    }
+
 }
