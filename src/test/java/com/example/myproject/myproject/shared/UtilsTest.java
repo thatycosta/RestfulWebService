476edda3bfe7bafae94ed3c67ffbfaa6ecfd8144
Tauane Costa <tauanedasilvacosta@gmail.com>
2020-06-23 18:22:06 -0300

    adding a integration text for the utils class

---
+++ src/test/java/com/example/myproject/myproject/shared/UtilsTest.java
@@ -0,0 +1,35 @@
+package com.example.myproject.myproject.shared;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UtilsTest {
+
+    @Autowired
+    Utils utils;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void generateUserId() {
+        String userId = utils.generateUserId(30);
+        String userId2 = utils.generateUserId(30);
+
+        assertNotNull(userId);
+        assertNotNull(userId2);
+
+        assertTrue(userId.length() == 30);
+        assertTrue(!userId2.equalsIgnoreCase(userId));
+    }
+}
