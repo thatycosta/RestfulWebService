6d838d891f95be93b7123ba7611dbe65eac69cd8
Tauane Costa <tauanedasilvacosta@gmail.com>
2020-07-27 18:35:18 -0300

    native sql query add

--- src/main/java/com/example/myproject/myproject/io/repositories/UserRepository.java
+++ src/main/java/com/example/myproject/myproject/io/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package com.example.myproject.myproject.io.repositories;
 
 import com.example.myproject.myproject.io.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
     UserEntity findByUserId(String userId);
+
+    @Query(value = "select * from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'", nativeQuery = true)
+    Page<UserEntity> findAllUsersWithConfirmedEmailAddress( Pageable pageableRequest);
 }
